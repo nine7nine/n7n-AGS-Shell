1d4a32969e5beab9a2d3a6e33ca0386728882f2a
jordan Johnston <johnstonljordan@gmail.com>
2024-03-28 14:28:25 -0500

    Workspaces: Re-implement Workspace Switcher in Panel
     -> Re-implement the Workspace Switcher in it's Panel Button(s).
    
     For whatever reason (personal preference?) Aylur dropped the ability
     to switch workspaces from the panel button, and instead it always
     will just open the switcher popup.
    
     Personally, I think it's a big regression in functionality, and I
     don't like that at all. To fix this:
    
      - Replace Widget.Label with Widget.Button
      - Use on_clicked() + dispatch for the buttons/workspaces
      - Drop using labels
      - Drop on_clicked() from PanelButton
    
     This effectively restores the old behaviour.
    
     -> Update and tweak bar.scss as necessary to make the new workspace
        buttons/widget styled properly.
    
    Signed-off-by: jordan Johnston <johnstonljordan@gmail.com>

--- ags/widget/bar/buttons/Workspaces.ts
+++ ags/widget/bar/buttons/Workspaces.ts
@@ -10,21 +10,18 @@ const dispatch = (arg: string | number) => {
 }
 
 const Workspaces = (ws: number) => Widget.Box({
-    children: range(ws || 20).map(i => Widget.Label({
+    children: range(ws || 20).map(i => Widget.Button({
         attribute: i,
-        vpack: "center",
-        label: `${i}`,
+        on_clicked: () => dispatch(i),
         setup: self => self.hook(hyprland, () => {
             self.toggleClassName("active", hyprland.active.workspace.id === i)
             self.toggleClassName("occupied", (hyprland.getWorkspace(i)?.windows || 0) > 0)
         }),
     })),
     setup: box => {
-        if (ws === 0) {
-            box.hook(hyprland.active.workspace, () => box.children.map(btn => {
-                btn.visible = hyprland.workspaces.some(ws => ws.id === btn.attribute)
-            }))
-        }
+        box.hook(hyprland.active.workspace, () => box.children.map(btn => {
+            btn.visible = hyprland.workspaces.some(ws => ws.id === btn.attribute)
+        }))
     },
 })
 
@@ -33,6 +30,6 @@ export default () => PanelButton({
     class_name: "workspaces",
     on_scroll_up: () => dispatch("m+1"),
     on_scroll_down: () => dispatch("m-1"),
-    on_clicked: () => App.toggleWindow("overview"),
     child: workspaces.bind().as(Workspaces),
 })
+
